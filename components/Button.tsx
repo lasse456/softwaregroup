f7902c68e18fc986dcdcb3aa7f13e7ece6209bc2
Lasse <lass.osmann@gmail.com>
2023-12-07 23:50:08 +0100

    Fixed stuff

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,9 @@
-const Button = (props) => {
+interface ButtonProps {
+  url: string;
+  message: string;
+}
+
+const Button = (props: ButtonProps) => {
   return (
     <a href={props.url}>
       <button className="bg-main px-[25px] py-[10px] text-white font-[600] text-[12px] rounded-[100px]">
